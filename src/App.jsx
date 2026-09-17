496dc19164cf76590d130309be4002abde97b231
Musrat001 <mohdmusrat73@gmail.com>
2026-09-18 00:38:02 +0530

    Created the First commit of ChatApp

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="bg-red-900">Chat App </h1>
+    </>
+  );
+}
+
+export default App;
